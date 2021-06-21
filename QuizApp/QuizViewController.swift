6477f8149002a6211d1d347ce6c84fb0c71d3f71
takanotakuya <tmw9898@icloud.com>
2021-06-21 10:46:36 +0900

    quizを画面に表示

--- QuizApp/QuizViewController.swift
+++ QuizApp/QuizViewController.swift
@@ -17,12 +17,23 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var answerButton4: UIButton!
     
     var csvArry: [String] = []
+    var quizArray: [String] = []
+    var quizCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         csvArry = loadCSV(fileName: "quiz")
         print(csvArry)
+        
+        quizArray = csvArry[quizCount].components(separatedBy: ",")
+        
+        quizNumberLabel.text = "第\(quizCount + 1)問"
+        quizTextView.text = quizArray[0]
+        answerButton1.setTitle(quizArray[2], for: .normal)
+        answerButton2.setTitle(quizArray[3], for: .normal)
+        answerButton3.setTitle(quizArray[4], for: .normal)
+        answerButton4.setTitle(quizArray[5], for: .normal)
         
     }
     
