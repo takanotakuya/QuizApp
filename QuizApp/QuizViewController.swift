1b6fb5525667d4927c132852c339183459c9a504
takanotakuya <tmw9898@icloud.com>
2021-06-21 10:29:25 +0900

    csv読み込み、配列に変換

--- QuizApp/QuizViewController.swift
+++ QuizApp/QuizViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class QuizViewController: UIViewController {
+    
+    var csvArry: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +18,19 @@ class QuizViewController: UIViewController {
     
     @IBAction func btnAction(sender: UIButton) {
         print(sender.tag)
+    }
+    
+    func loadCSV(fileName: String) -> [String] {
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            let csvData = try String(contentsOfFile: csvBundle, encoding: String.Encoding.utf8)
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            csvArry = lineChange.components(separatedBy: "\n")
+            csvArry.removeLast()
+        } catch {
+            print("エラー")
+        }
+        return csvArry
     }
 
 }
