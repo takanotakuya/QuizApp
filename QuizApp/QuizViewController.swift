06e9628f5037ba9bfd5f7835b84f6c8090c5b46f
takanotakuya <tmw9898@icloud.com>
2021-06-21 11:37:14 +0900

    問題終了後スコア画面に遷移

--- QuizApp/QuizViewController.swift
+++ QuizApp/QuizViewController.swift
@@ -16,17 +16,17 @@ class QuizViewController: UIViewController {
     @IBOutlet weak var answerButton3: UIButton!
     @IBOutlet weak var answerButton4: UIButton!
     
-    var csvArry: [String] = []
+    var csvArray: [String] = []
     var quizArray: [String] = []
     var quizCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        csvArry = loadCSV(fileName: "quiz")
-        print(csvArry)
+        csvArray = loadCSV(fileName: "quiz")
+        print(csvArray)
         
-        quizArray = csvArry[quizCount].components(separatedBy: ",")
+        quizArray = csvArray[quizCount].components(separatedBy: ",")
         
         quizNumberLabel.text = "第\(quizCount + 1)問"
         quizTextView.text = quizArray[0]
@@ -48,13 +48,17 @@ class QuizViewController: UIViewController {
     
     func nextQuiz() {
         quizCount += 1
-        quizArray = csvArry[quizCount].components(separatedBy: ",")
-        quizNumberLabel.text = "第\(quizCount + 1)問"
-        quizTextView.text = quizArray[0]
-        answerButton1.setTitle(quizArray[2], for: .normal)
-        answerButton2.setTitle(quizArray[3], for: .normal)
-        answerButton3.setTitle(quizArray[4], for: .normal)
-        answerButton4.setTitle(quizArray[5], for: .normal)
+        if quizCount < csvArray.count {
+            quizArray = csvArray[quizCount].components(separatedBy: ",")
+            quizNumberLabel.text = "第\(quizCount + 1)問"
+            quizTextView.text = quizArray[0]
+            answerButton1.setTitle(quizArray[2], for: .normal)
+            answerButton2.setTitle(quizArray[3], for: .normal)
+            answerButton3.setTitle(quizArray[4], for: .normal)
+            answerButton4.setTitle(quizArray[5], for: .normal)
+        } else {
+            performSegue(withIdentifier: "toScoreVC", sender: nil)
+        }
     }
     
     func loadCSV(fileName: String) -> [String] {
@@ -62,12 +66,12 @@ class QuizViewController: UIViewController {
         do {
             let csvData = try String(contentsOfFile: csvBundle, encoding: String.Encoding.utf8)
             let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
-            csvArry = lineChange.components(separatedBy: "\n")
-            csvArry.removeLast()
+            csvArray = lineChange.components(separatedBy: "\n")
+            csvArray.removeLast()
         } catch {
             print("エラー")
         }
-        return csvArry
+        return csvArray
     }
 
 }
